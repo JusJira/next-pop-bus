472d6829a0189d906c0830be23471a7bb0808567
JustinJira <jus.jira@gmail.com>
2023-09-28 09:53:56 +0700

    style: grid 😢

--- components/timeBox.tsx
+++ components/timeBox.tsx
@@ -22,7 +22,8 @@ function TimeBox(props: { time: string[] }) {
   });
   if (next) {
     return (
-      <div className="flex w-full flex-row flex-wrap gap-2 items-center justify-center">
+      <div className="flex w-full flex-row items-center justify-center">
+        <div className="grid gap-2 grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-12 xl:grid-cols-[repeat(16,_minmax(0,_1fr));]">
         {ctime.map((time, index) => (
           <div
             key={index}
@@ -38,6 +39,7 @@ function TimeBox(props: { time: string[] }) {
             {/* {next?.format('HH:mm').toString()} */}
           </div>
         ))}
+        </div>
       </div>
     );
   }
