84b85a53c991e2bfb5e9ac545674e84e7583b24b
JustinJira <jus.jira@gmail.com>
2023-09-28 11:02:22 +0700

    fix: remove useState

--- components/clock.tsx
+++ components/clock.tsx
@@ -1,8 +1,3 @@
-"use client";
-
-import dynamic from 'next/dynamic'
-
-import React, { useEffect, useState } from "react";
 import { DateTime } from "luxon";
 
 function Clock() {
@@ -10,10 +5,8 @@ function Clock() {
     .setZone("Asia/Bangkok")
     .toFormat("HH:mm")
     .toString();
-  const [time, setTime] = useState("");
-  useEffect(() => setTime(now), [now]);
 
-  return <span className="text-lg">Last Updated: {time}</span>;
+  return <span className="text-lg">Last Updated: {now}</span>;
 }
 
 export default Clock;
