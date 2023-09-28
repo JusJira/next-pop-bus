3baa281acdbe61affc035c2ce054fa24d3074764
JustinJira <jus.jira@gmail.com>
2023-09-28 10:13:08 +0700

    style: grid fixed yay🥹

--- components/timeBox.tsx
+++ components/timeBox.tsx
@@ -23,7 +23,7 @@ function TimeBox(props: { time: string[] }) {
   if (next) {
     return (
       <div className="flex w-full flex-row items-center justify-center">
-        <div className="grid gap-2 grid-cols-4 sm:grid-cols-6 md:grid-cols-8 lg:grid-cols-12 xl:grid-cols-[repeat(16,_minmax(0,_1fr));]">
+        <div className="container">
         {ctime.map((time, index) => (
           <div
             key={index}
