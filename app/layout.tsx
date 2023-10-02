50fa201138708ac3441aa2344d3b13f18c9ee690
JustinJira <jus.jira@gmail.com>
2023-10-02 09:14:22 +0700

    style: added favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -22,6 +22,26 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body
         className={`${inter.className} dark:bg-neutral-900 bg-neutral-300 `}
       >
@@ -52,11 +72,17 @@ export default function RootLayout({
                   </Link>
                 </span>
                 <span className="w-full flex flex-row items-center justify-center gap-1">
-                  <Link href="https://instagram.com/Jus.Jira" aria-label="Instagram Link">
+                  <Link
+                    href="https://instagram.com/Jus.Jira"
+                    aria-label="Instagram Link"
+                  >
                     <Instagram />
                   </Link>
 
-                  <Link href="https://github.com/jusjira/next-pop-bus" aria-label="Github Link">
+                  <Link
+                    href="https://github.com/jusjira/next-pop-bus"
+                    aria-label="Github Link"
+                  >
                     <Github />
                   </Link>
                 </span>
