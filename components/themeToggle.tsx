17ca1f2d7271ad9397250ecc08fc19e5d6048b65
JustinJira <jus.jira@gmail.com>
2023-11-30 08:53:31 +0700

    fix: Theme toggle

--- components/themeToggle.tsx
+++ components/themeToggle.tsx
@@ -4,43 +4,24 @@ import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
 import { Switch } from "@/components/ui/switch";
 import { Label } from "@/components/ui/label";
-import { Button } from "@/components/ui/button"
-  import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuTrigger,
-  } from "@/components/ui/dropdown-menu"
-
-export function DarkModeToggle() {
-  const { theme, setTheme } = useTheme();
-  const handleTheme = (state: boolean) => {
-    if (state) {
-      setTheme("dark");
-    } else {
-      setTheme("light");
-    }
-  };
-
-  return (
-    <div className="flex h-10 w-2/3 flex-row items-center justify-between rounded-md border-[1px] border-black bg-white px-2 shadow-2xl dark:bg-black lg:w-48">
-      <span className="sr-only">Dark mode toggle</span>
-      <Sun className="h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0" />
-      <Moon className="absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100" />
-      <Label htmlFor="dark-mode">
-        {theme == "dark" ? "Dark Mode" : "Light Mode"}
-      </Label>
-      <Switch
-        id="dark-mode"
-        checked={theme == "dark"}
-        onCheckedChange={(state) => handleTheme(state)}
-      />
-    </div>
-  );
-}
+import { Button } from "@/components/ui/button";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { useEffect, useState } from "react";
 
 export function ModeToggle() {
+  const [mounted, setMounted] = useState(false);
   const { setTheme } = useTheme();
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <div className="w-10 h-10"></div>;
+  }
 
   return (
     <DropdownMenu>
